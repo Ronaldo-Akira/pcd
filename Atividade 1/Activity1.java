4e73161716348dc7bd774d7df03a729bc0244851
Ronaldo Akira Fujigaki Kinoshita <ronaki.kinoshita@gmail.com>
2021-11-04 00:47:09 -0300

    Add AnalysisResults and csv Analysis

--- Atividade 1/Activity1.java
+++ Atividade 1/Activity1.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Activity1 {
 
@@ -6,14 +10,13 @@ public class Activity1 {
         Utility.rPetomino(table, 10, 30);
     }
 
-    private static void highLife(int nThreads, int gridSize, int iterations){
+    private static AnalysisResults highLife(int nThreads, int gridSize, int iterations) {
         int[][] grid = new int[gridSize][gridSize];
         int[][] newGrid = new int[gridSize][gridSize];
         init(grid);
         Thread[] threadArray = new Thread[nThreads];
+        final long startTime = System.currentTimeMillis();
         for (int i = 0; i <= iterations; i++) {
-            System.out.println("Celulas[" +  i +  "]:" + Utility.countLivingCells(grid));
-
             int beginIndex = 0, endIndex = gridSize / nThreads;
             for (int j = 0; j < nThreads; j++) {
                 threadArray[j] = new HighLife(grid, newGrid, beginIndex, endIndex);
@@ -30,16 +33,18 @@ public class Activity1 {
             }
             Utility.copyMatrix(grid, newGrid);
         }
-        System.out.println("Celulas: " + Utility.countLivingCells(grid));
+        final long endTime = System.currentTimeMillis();
+        return new AnalysisResults(nThreads, endTime - startTime, Utility.countLivingCells(grid));
     }
 
-    private static void gameOfLife(int nThreads, int gridSize, int iterations){
+    private static AnalysisResults gameOfLife(int nThreads, int gridSize, int iterations) {
         int[][] grid = new int[gridSize][gridSize];
         int[][] newGrid = new int[gridSize][gridSize];
         init(grid);
         Thread[] threadArray = new Thread[nThreads];
+        final long startTime = System.currentTimeMillis();
         for (int i = 0; i <= iterations; i++) {
-            System.out.println("Celulas[" +  i +  "]:" + Utility.countLivingCells(grid));
+//            System.out.println("Celulas[" + i + "]:" + Utility.countLivingCells(grid));
             int beginIndex = 0, endIndex = gridSize / nThreads;
             for (int j = 0; j < nThreads; j++) {
                 threadArray[j] = new GameOfLife(grid, newGrid, beginIndex, endIndex);
@@ -54,15 +59,63 @@ public class Activity1 {
                     e.printStackTrace();
                 }
             }
+//            System.out.println("Grid " + i);
+//            Utility.printTable(grid);
             Utility.copyMatrix(grid, newGrid);
         }
-        System.out.println("Celulas: " + Utility.countLivingCells(grid));
+        final long endTime = System.currentTimeMillis();
+//        System.out.println("Celulas: " + Utility.countLivingCells(grid));
+        return new AnalysisResults(nThreads, endTime - startTime, Utility.countLivingCells(grid));
+    }
+
+
+    private static void writeToCsv(String path, ArrayList<AnalysisResults> results) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path), StandardCharsets.UTF_8));
+        String header = "Threads,Time,Result\n";
+        bw.write(header);
+        for (AnalysisResults result : results) {
+            String oneLine = result.getThreads() + "," +
+                    result.getTime() + "," +
+                    result.getLivingCells();
+            bw.write(oneLine);
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    private static void analysisGameOfLife(int n) {
+        ArrayList<AnalysisResults> results = new java.util.ArrayList<>(Collections.emptyList());
+        for (int i = 0; i < n; i++) {
+            results.add(gameOfLife(1, 2048, 2000));
+            results.add(gameOfLife(2, 2048, 2000));
+            results.add(gameOfLife(4, 2048, 2000));
+            results.add(gameOfLife(8, 2048, 2000));
+        }
+        try {
+            writeToCsv("results-GameOfLife.csv", results);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void analysisHighLife(int n) {
+        ArrayList<AnalysisResults> results = new java.util.ArrayList<>(Collections.emptyList());
+        for (int i = 0; i < n; i++) {
+            results.add(highLife(1, 2048, 2000));
+            results.add(highLife(2, 2048, 2000));
+            results.add(highLife(4, 2048, 2000));
+            results.add(highLife(8, 2048, 2000));
+        }
+        try {
+            writeToCsv("results-HighLife.csv", results);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        final long startTime = System.currentTimeMillis();
-        highLife(2, 2048, 15);
-        final long endTime = System.currentTimeMillis();
-        System.out.println("O tempo em millisecond foi de : " + (endTime - startTime));
+//        analysisHighLife(5);
+        analysisGameOfLife(5);
     }
 }
