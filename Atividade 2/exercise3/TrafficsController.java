bc65819c044b1557c0c6ec6c22dd077189a402c3
Ronaldo Akira Fujigaki Kinoshita <ronaki.kinoshita@gmail.com>
2021-11-23 23:28:55 -0300

    Feature: implements semaphore in the critical section

--- Atividade 2/exercise3/TrafficsController.java
+++ Atividade 2/exercise3/TrafficsController.java
@@ -1,17 +1,33 @@
 package exercise3;
 
+import java.util.concurrent.Semaphore;
+
 public class TrafficsController {
 
+    Semaphore semaphore = new Semaphore(1);
+
     public void enterLeft() {
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void enterRight() {
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void leaveLeft() {
+        semaphore.release();
     }
 
     public void leaveRight() {
+        semaphore.release();
     }
 
 }
